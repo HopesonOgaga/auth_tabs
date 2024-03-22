83200d3f936caf97342bc23c209fc14bb8d2f331
Hopeson <hopesonogaga@gmail.com>
2024-03-22 15:09:36 +0100

    mobile view

--- build/js/pick.js
+++ build/js/pick.js
@@ -1,15 +1,16 @@
 const form = document.getElementById("form");
 
-form.addEventListener("click", function (e) {
-  e.preventDefault();
-  let test = [];
-  // loop and get value from the site 
-  document.querySelectorAll('[type="checkbox"]').forEach((item) => {
-    if (item.checked === true) {
-      test.push(item.value);
-    }
-  });
-  console.log(test);
-});
 
-// 
+form.addEventListener("click", function (e) {
+  e.stopPropagation();
+  if (e.target !== form) {
+    let test = [];
+    // loop and get value from the site
+    document.querySelectorAll('[type="checkbox"]').forEach((item) => {
+      if (item.checked === true) {
+        test.push(item.value);
+      }
+    });
+    console.log(test);
+  }
+});
