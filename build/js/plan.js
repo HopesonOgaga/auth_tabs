50a1a286ad1955069476286fd7918bd65e923498
Hopeson <hopesonogaga@gmail.com>
2024-03-25 12:01:05 +0100

    ui plan fixes

--- build/js/plan.js
+++ build/js/plan.js
@@ -4,11 +4,12 @@ const advance = document.getElementById("advance");
 const pro = document.getElementById("pro");
 const next_button = document.getElementById("next");
 const toggle_btn = document.getElementById("toggle-btn");
-//
+
+// Select elements with correct classes
 const arcade_free = document.querySelector(".arcade-free");
 const arcade_advance = document.querySelector(".arcade-advance");
 const arcade_pro = document.querySelector(".arcade-pro");
-const toggle = document.querySelector(".toggle__input");
+
 // value data
 const info_val = {
   pro_value: 15,
@@ -34,22 +35,25 @@ pro.addEventListener("click", function () {
   console.log(value);
 });
 
-//toggle button
-toggle.addEventListener("change", function () {
-  // Checking if the toggle is checked
-  if (toggle.checked) {
-    arcade_free.classList.remove("hidden"); // Remove the 'hidden' class to show the element
-    arcade_advance.classList.remove("hidden");
-    arcade_pro.classList.remove("hidden");
-  } else {
-    arcade_free.classList.add("hidden"); // Add the 'hidden' class to hide the element
-    arcade_advance.classList.add("hidden");
-    arcade_pro.classList.add("hidden");
-  }
-});
+//toggle_btn button
+if (toggle_btn) {
+  toggle_btn.addEventListener("change", function (e) {
+    e.stopPropagation()
+    // Checking if the toggle_btn is checked
+    if (toggle_btn.checked) {
+      arcade_free.classList.remove("hidden"); // Remove the 'hidden' class to show the element
+      arcade_advance.classList.remove("hidden");
+      arcade_pro.classList.remove("hidden");
+    } else {
+      arcade_free.classList.add("hidden"); // Add the 'hidden' class to hide the element
+      arcade_advance.classList.add("hidden");
+      arcade_pro.classList.add("hidden");
+    }
+  });
+}
 
-next_button.addEventListener("click", function () {
-  location.href = "./pick.html";
-});
-
-//
+if (next_button) {
+  next_button.addEventListener("click", function () {
+    location.href = "./pick.html";
+  });
+}
